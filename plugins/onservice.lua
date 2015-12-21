ec8c7fc0acc3421aae2afcf59071b58d897f5393
commanderbotwili <shayanmayeli@yahoo.com>
2015-12-21 20:40:06 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -10,19 +10,11 @@ local bot_id = 133851174 -- your bot id
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
-if matches[1] == 'left' and is_admin(msg) then
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    end
  
 return {
   patterns = {
     "^[!/$&#@]([Ll]eave)$",
-    "^[!/$&#@]([Ll]eft)$"
-    "^([Ll]eft)$",
-    "^[Ll]eave)$"
+    "^([Ll]eave)$",
     "^!!tgservice (.+)$",
   },
   run = run

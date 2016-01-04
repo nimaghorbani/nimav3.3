2eddbd09d0fde3f1df36118c2734de97ec5c7ce7
kossher <love_shayan13@yahoo.com>
2016-01-04 21:58:10 +0330

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,6 @@
 do local _ = {
-  about_text = "Admins\n@Blackteam_5_00\nSpecial thanks to\n@WilSoN_DeVeLoPeR\n",
+  about_text = "W_SaTaN_W V5.5.2\nAdmins\n@WilSoN_DeVeLoPeR\nSpecial thanks to\n@D
+awsh_Alfred\nour channel\n@W_SaTaN_w_chnnl\n",
   disabled_channels = {},
   enabled_plugins = {
     "onservice",
@@ -20,17 +21,19 @@ do local _ = {
     "helprealm",
     "plugins",
     "block",
-    "tagall"
+    "tagall",
+    "Feedback",
+    "Cover"
   },
-  help_text = "Commands list :\n\n!kick [username|id]\nYou can also do it by reply\n\n!ban [ username|id]\nYou can also do it by reply\n\n!unban [id]\nYou can also do it by reply\n\n!who\nMembers list\n\n!modlist\nModerators list\n\n!promote [username]\nPromote someone\n\n!demote [username]\nDemote someone\n\n!kickme\nWill kick user\n\n!about\nGroup description\n\n!setphoto\nSet and locks group photo\n\n!setname [name]\nSet group name\n\n!rules\nGroup rules\n\n!id\nreturn group id or user id\n\n!help\n\n!lock [member|name|bots]\nLocks [member|name|bots] \n\n!unlock [member|name|photo|bots]\nUnlocks [member|name|photo|bots]\n\n!set rules <text>\nSet <text> as rules\n\n!set about <text>\nSet <text> as about\n\n!settings\nReturns group settings\n\n!newlink\ncreate/revoke your group link\n\n!link\nreturns group link\n\n!owner\nreturns group owner id\n\n!setowner [id]\nWill set id as owner\n\n!setflood [value]\nSet [value] as flood sensitivity\n\n!stats\nSimple message statistics\n\n!save [value] <text>\nSave <text> as [value]\n\n!get [value]\nReturns text of [value]\n\n!clean [modlist|rules|about]\nWill clear [modlist|rules|about] and set it to nil\n\n!res [username]\nreturns user id\n\"!res @username\"\n\n!log\nwill return group logs\n\n!banlist\nwill return group ban list\n\n**U can use both \"/\" and \"!\" \n\n\n*Only owner and mods can add bots in group\n\n\n*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands\n\n*Only owner can use res,setowner,promote,demote and log commands\n",
+  help_text = "Commands list :\n\n!kick [username|id]\nYou can also do it by reply\n\n!ban [ username|id]\nYou can also do it by reply\n\n!unban [id]\nYou can also do it by reply\n\n!who\nMembers list\n\n!modlist\nModerators list\n\n!promote [username]\nPromote someone\n\n!demote [username]\nDemote someone\n\n!kickme\nWill kick user\n\n!about\nGroup description\n\n!setphoto\nSet and locks group photo\n\n!setname [name]\nSet group name\n\n!rules\nGroup rules\n\n!id\nreturn group id or user id\n\n!help\n\n!lock [member|name|bots]\nLocks [member|name|bots]\n\n!unlock [member|name|photo|bots]\nUnlocks [member|name|photo|bots]\n\n!set rules <text>\nSet <text> as rules\n\n!set about <text>\nSet <text> as about\n\n!settings\nReturns group settings\n\n!newlink\ncreate/revoke your group link\n\n!link\nreturns group link\n\n!owner\nreturns group owner id\n\n!setowner [id]\nWill set id as owner\n\n!setflood [value]\nSet [value] as flood sensitivity\n\n!stats\nSimple message statistics\n\n!save [value] <text>\nSave <text> as [value]\n\n!get [value]\nReturns text of [value]\n\n!clean [modlist|rules|about]\nWill clear [modlist|rules|about] and set it to nil\n\n!res [username]\nreturns user id\n\"!res @username\"\n\n!log\nwill return group logs\n\n!banlist\nwill return group ban list\n\n**U can use both \"/\" and \"!\" \n\n\n*Only owner and mods can add bots in group\n\n\n*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands\n\n*Only owner can use res,setowner,promote,demote and log commands\n\nCreator: @WilSoN_DeVeLoPeR\n",
   moderation = {
     data = "data/moderation.json"
   },
   realm = {
-    63412073
+    53167206
   },
   sudo_users = {
-    171670873
+    94704012
   }
 }
 return _

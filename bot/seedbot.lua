c9834f56ef4bc0641574d78e1a9172169b0574cb
kossher <love_shayan13@yahoo.com>
2016-01-04 22:38:00 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,9 +226,9 @@ function create_config( )
     "Feedback",
     "Cover"
     },
-    sudo_users = {107032464},--Sudo users
+    sudo_users = {94704012},--Sudo users
     disabled_channels = {},
-    realm = {63412073},--Realms Id
+    realm = {53167206},--Realms Id
     moderation = {data = 'data/moderation.json'},
     about_text = [[W_SaTaN_W V5.5.2
     Admins
